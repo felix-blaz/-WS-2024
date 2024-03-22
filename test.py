bc8f760ced778dfce5cabe079811c96c8d80055d
felix-blaz <74864332+felix-blaz@users.noreply.github.com>
2024-03-22 23:45:50 +0000

    tt

--- test.py
+++ test.py
@@ -27,23 +27,39 @@ def checkServiceForWord(url, keyword):
 class TestStringMethods(unittest.TestCase):
 
 
-    def test_getProductsKeyword(self):
+    def test_getProductsEndpoint(self):
         url = 'http://localhost:5000/getProducts'
         result = checkServiceForWord(url, '1')
         self.assertTrue(result)
         saveResult('Test1', url, result)
 
+    def test_getTitlesEndpoint(self):
+        url = 'http://localhost:5000/getTitles'
+        result = checkServiceForWord(url, 'bread')
+        self.assertTrue(result)
+        saveResult('Test2', url, result)
+
     def test_rootEndpointKeyword(self):
         url = 'http://localhost:5000/'
         result = checkServiceForWord(url, 'Will')
         self.assertTrue(result)
-        saveResult('Test2', url, result)
+        saveResult('Test3', url, result)
 
 
-def test_rootEndpointKeyword(self):
-    url = 'http://localhost:5000/'
-    result = checkServiceForWord(url, 'Will')
-    self.assertTrue(result)
-    saveResult('Test2', url, result)
+    def test_insertProductsEndpointKeyword(self):
+        url = 'http://localhost:5000/insertProducts'
+        headers = {'Content-Type': 'application/json', 'api_key': 'aaa'}
+        data = {
+            'ProductId': '123',
+            'Title': 'Test Product',
+            'Quantity': 10
+        }
+
+        response = requests.post(url, headers=headers, json=data)
+        # Check if the request was successful (status code 201)
+        self.assertEqual(response.status_code, 201)
+
+        # Save the test result
+        saveResult('Test4', url, response.status_code)
 if __name__ == '__main__':
     unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='test_output'))
