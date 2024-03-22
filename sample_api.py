11a7411bbeffe9eeb626238898cf972bb0127665
felix-blaz <74864332+felix-blaz@users.noreply.github.com>
2024-03-22 22:45:58 +0000

    jj

--- sample_api.py
+++ sample_api.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 import json
+import graphene
 from pymongo import MongoClient
 from bson.json_util import dumps, loads
 from bson.objectid import ObjectId
@@ -9,32 +10,86 @@ from bson.objectid import ObjectId
 app = Flask(__name__)
 api = Api(app)
 
+client = MongoClient("mongodb://root:example@localhost:27017/")
+db = client.sales
+collection = db.sales_data
+API_KEY = "aaa"
+
 class GetProducts(Resource):
 
     def get(self):
-        client = MongoClient("mongodb://root:example@localhost:27017/")
-        db = client.sales
-        collection = db.sales_data
         results = dumps(collection.find())
         return json.loads(results)
 
+
+class Sale(graphene.ObjectType):
+    Title = graphene.String()
+
+
+class Query(graphene.ObjectType):
+    titles = graphene.List(graphene.String)
+
+    def resolve_titles(root, info):
+        results = collection.find()
+        titles = [item['Title'] for item in results]
+        return titles
+
+
+schema = graphene.Schema(query=Query)
+
+
 class GetTitles(Resource):
     def get(self):
-        client = MongoClient("mongodb://root:example@localhost:27017/")
-        db = client.sales
-        collection = db.sales_data
-        results = dumps(collection.find({"ProductId": 3}))
+        result = schema.execute('{ titles }')
+        data_json = json.dumps(result.data)
+        return data_json
 
-        print(results)
-        print(type(results))
-        return json.loads(results)
+class InsertProducts(Resource):
+    def post(self):
+        # Check if API key is provided in the request URL
+        api_key = request.args.get('api_key')
+        if api_key != API_KEY:
+            return jsonify({'error': 'Invalid API key'}), 401
 
-             
+        # Parse request data
+        data = request.json
+        product_id = data.get('ProductId')
+        title = data.get('Title')
+        quantity = data.get('Quantity')
+
+        # Validate request data
+        if not all([product_id, title, quantity]):
+            return jsonify({'error': 'Missing required fields'}), 400
+
+        # Store data in MongoDB
+        try:
+            product = {
+                'ProductId': product_id,
+                'Title': title,
+                'Quantity': quantity
+            }
+            result = collection.insert_one(product)
+            return jsonify({'message': 'Product added successfully', 'inserted_id': str(result.inserted_id)}), 201
+        except Exception as e:
+            return jsonify({'error': f'Failed to insert product: {str(e)}'}), 500
+
+
+
+class Root(Resource):
+    def get(self):
+        endpoints = {
+            '/getProducts': 'Will retrieve all the products in the mongo db\n',
+            '/insertProducts': 'Will allow the user to add items into the momgo database as long as they have the api key. '
+                               'This is done using postman\n',
+            '/getTitles': 'Will allow the user to retrieve all the titles of the products in the database using graphql'
+        }
+
+        return jsonify(endpoints)
+
+api.add_resource(Root, '/')
+api.add_resource(InsertProducts, '/insertProducts')
 api.add_resource(GetProducts, '/getProducts')
 api.add_resource(GetTitles, '/getTitles')
 
-
-
-            
 if __name__ == '__main__':
     app.run(debug=True)
--- test.py
+++ test.py
@@ -3,29 +3,47 @@ import unittest
 import requests
 
 
-
+def saveResult(name, url, result):
+    with open('test.log', 'w+') as f:
+        f.write('Test name:' + str(name) + '\n')
+        f.write('Test URL:' + str(url) + '\n')
+        f.write('Test result:' + str(result) + '\n')
+        f.write('---------------------------------------------\n ')
 def checkServiceForWord(url, keyword):
+    result = False
     try:
         x = requests.get(url)
         print(x.text)
         serverStatus=1
         if keyword in x.text:
             print("found keyword")
-        return True
+            result=True
     except:
         print("error")
-        return False
+        result= False
+    return result
 
 
 class TestStringMethods(unittest.TestCase):
-    def test_twoValuesAreEqual(self):
-        value1=True
-        #value2=True
 
+
+    def test_getProductsKeyword(self):
         url = 'http://localhost:5000/getProducts'
         result = checkServiceForWord(url, '1')
-        print(result)
+        self.assertTrue(result)
+        saveResult('Test1', url, result)
 
-        self.assertEqual(value1, result)
+    def test_rootEndpointKeyword(self):
+        url = 'http://localhost:5000/'
+        result = checkServiceForWord(url, 'Will')
+        self.assertTrue(result)
+        saveResult('Test2', url, result)
+
+
+def test_rootEndpointKeyword(self):
+    url = 'http://localhost:5000/'
+    result = checkServiceForWord(url, 'Will')
+    self.assertTrue(result)
+    saveResult('Test2', url, result)
 if __name__ == '__main__':
     unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='test_output'))
